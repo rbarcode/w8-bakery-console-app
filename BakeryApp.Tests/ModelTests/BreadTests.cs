2f402363cd6316bd9cae4b85d09246494e2c21df
Richard Barbour <richyb07@gmail.com>
2023-09-22 10:28:40 -0700

    Write and pass GetOrder test in BreadTests.cs.

--- BakeryApp.Tests/ModelTests/BreadTests.cs
+++ BakeryApp.Tests/ModelTests/BreadTests.cs
@@ -3,5 +3,16 @@ using BakeryApp.Models;
 
 namespace BakeryApp.Tests
 {
-  
+  [TestClass]
+  public class BreadTests
+
+  {
+    [TestMethod]
+    public void GetOrder_GetsOrder_Void()
+    {
+      int newOrder = 6;
+      Bread.CalcBreadPrice(newOrder);
+      Assert.AreEqual(newOrder, Bread.Order);
+    }
+  }
 }
